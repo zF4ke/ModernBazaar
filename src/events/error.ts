bd3dc4a521cd9458e711e01a17cb40182d89fc09
Pedro Silva <pedrohsilva955@gmail.com>
2025-07-24 06:52:53 +0100

    moves legacy code to bot/

--- src/events/error.ts
+++
@@ -1,8 +0,0 @@
-import { Events } from "discord.js";
-import { ExtendedClient } from "../types";
-
-export function setupErrorEvent(client: ExtendedClient) {
-    client.on(Events.Error, error => {
-        console.error('❌ Discord client error:', error);
-    });
-}
