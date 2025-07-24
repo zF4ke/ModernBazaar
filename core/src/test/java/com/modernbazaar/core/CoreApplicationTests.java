9d99b98c706dc7f543d5ee71cc1c510739e90eff
Pedro Silva <pedrohsilva955@gmail.com>
2025-07-24 09:47:38 +0100

    chore(monitoring): configures Grafana and Prometheus. Fixes environment variables.

--- core/src/test/java/com/modernbazaar/core/CoreApplicationTests.java
+++ core/src/test/java/com/modernbazaar/core/CoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.modernbazaar.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test") // Use the "test" profile for testing
 class CoreApplicationTests {
 
     @Test
